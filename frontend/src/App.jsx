c004a73a8f659569188020f723737633d463c584
Fabiola Li Wu <fabiolaliwu@users.noreply.github.com>
2025-05-12 00:58:06 -0400

    little change in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,61 +23,56 @@ function HomePage({ detectedLetter, setDetectedLetter, letters, setLetters }) {
         <div className='logo'> 
           <h3>ASL Translator</h3>
         </div>
-        <div style={{ margin: '10px' }}>
-          <Link to="/quiz">
-            <button style={{
-                  padding: "8px 16px",
-                  fontSize: "1rem",
-                  backgroundColor: "#f1d6ff",
-                  border: "1px solid black",
-                  borderRadius: "5px",
-                  cursor: "pointer",
-                  marginBottom: "10px",
-                }}>Take ASL Quiz</button>
-          </Link>
-        </div>
         <div className='cameraBody'>
           <CameraFeed setLetters={setLetters}/>
-        </div>
-        <div style={{ textAlign: "center"}}>
-          <div style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
-            Detected Letters:
-          </div>
-          <div style={{ fontSize: "1.5rem", marginTop: "10px" }}>
-            {letters.join(" ")}
-            <div style={{ marginTop: "15px" }}>
-              <button
-                onClick={handleBackspace}
-                style={{
-                  padding: "8px 16px",
-                  fontSize: "1rem",
-                  backgroundColor: "#ff9999",
-                  border: "1px solid black",
-                  borderRadius: "5px",
-                  cursor: "pointer",
-                  marginRight: "10px",
-                  marginTop: "10px",
-                }}
-              >
-                ⌫ DELETE
-              </button>
-              <button
-                onClick={handleClearAll}
-                style={{
-                  padding: "8px 16px",
-                  fontSize: "1rem",
-                  backgroundColor: "#ccffcc",
-                  border: "1px solid black",
-                  borderRadius: "5px",
-                  cursor: "pointer",
-                  marginTop: "10px",
-                }}
-              >
-                CLEAR
-              </button>
+          <div className='letterPart'>
+            <div style={{ fontSize: "1.5rem", fontWeight: "bold", width: "100%"}}>
+              detected letters
             </div>
+            <div className="outputLetters">
+              {letters.join(" ")}
+            </div>
+            <div className="letter-actions">
+                <button
+                  onClick={handleBackspace}
+                  style={{
+                    padding: "8px 16px",
+                    fontSize: "1rem",
+                    backgroundColor: "#ff9999",
+                    border: "1px solid black",
+                    borderRadius: "5px",
+                    cursor: "pointer",
+                    marginRight: "10px",
+                    marginTop: "10px",
+                  }}
+                >
+                  ⌫ DELETE
+                </button>
+                <button
+                  onClick={handleClearAll}
+                  style={{
+                    padding: "8px 16px",
+                    fontSize: "1rem",
+                    backgroundColor: "#ccffcc",
+                    border: "1px solid black",
+                    borderRadius: "5px",
+                    cursor: "pointer",
+                    marginTop: "10px",
+                  }}
+                >
+                  CLEAR
+                </button>
+              </div>
           </div>
         </div>
+        
+      </div>
+      <div className="quiz-float-btn">
+        <Link to="/quiz">
+          <button>
+            TAKE ASL QUIZ
+          </button>
+        </Link>
       </div>
     </>
   )
